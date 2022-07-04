1de4e473e06adf9004a704d2dc14fe43fe041e7d
Thomas Crescenzi <trescenzi@gmail.com>
2022-07-03 20:25:33 -0400

    refactor: break stuff out

---
+++ r_command.ts
@@ -0,0 +1,21 @@
+import { DiceRoll } from 'https://cdn.skypack.dev/@dice-roller/rpg-dice-roller';
+import {SlashCommand} from './handle_command.ts';
+
+export type DiceCommand = {
+  name: 'dice',
+  value: string,
+};
+
+export const RCommand : SlashCommand<[DiceCommand]>  = {
+  name: 'r',
+  handler: (options) => {
+    const {value} = options[0];
+    const roll = new DiceRoll(value);
+    return {
+      data: {
+        content: roll.output,
+      },
+      type: 4,
+    }
+  }
+}
